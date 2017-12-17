4251356c2d4509c54f6bc60f9fadb5d19911ed16
DReflex <tkd.vatra@hotmail.com>
2017-12-17 13:01:45 +0100

    creating skeleton

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './home/home';
 import Todo from './todo/todo';
+import Welcome from './welcome/welcome'
+import User from './user/user'
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './navbar/navbar'
-// export main to seperate component if?
+
 class App extends React.Component{
   render(){
     return(
@@ -11,8 +13,10 @@ class App extends React.Component{
         <Navbar />
         <main>
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route exact path='/' component={Welcome} />
+            <Route path='/home'component = {Home} />
             <Route  path='/todo' component={Todo} />
+            <Route path='/user' component = {User} />
           </Switch>
         </main>
       </div>
---
+++ src/app/user/user.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class User extends React.Component{
+  render(){
+    return(
+      <div><h1>IM user </h1></div>
+    )
+  }
+}
+export default User
