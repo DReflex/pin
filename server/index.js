9d06b74142fdd0d1c4eca50f93f11f9c782aeac5
DReflex <tkd.vatra@hotmail.com>
2017-12-20 01:13:09 +0100

    pushing to heroku

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.use('/api', require('./routes/api'));
 //init app
 //build part of the react app
 //uncoment this after npm build
-/*app.use('/', express.static(path.join(__dirname, '../build')));
+app.use('/', express.static(path.join(__dirname, '../build')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
-*/
+
 //err
 app.use(function(err, req, res, next){
   //console.log(err);
